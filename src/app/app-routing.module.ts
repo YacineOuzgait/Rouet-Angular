440e4e530f566737236a0485ffa35472b83f6137
Yacine Ouzgait <yacineouzgait59146@gmail.com>
2022-12-09 10:42:29 +0100

    corection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
-import { SignupComponent } from './menu/signup/signup.component';
-import { UserProfileComponent } from './menu/user-profile/user-profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+
 
 
 const routes: Routes = [
+
   {
-    path:'menu',
-    component: MenuComponent,
-    children: [
-      {
-        path:'', 
-        redirectTo: 'user'
-      },
-      {
-        path:'user', 
-        component:UserProfileComponent
-      },
-      {
-        path:'signup', 
-        component:SignupComponent
-      }
-    ]
+    path: 'user',
+    component: UserProfileComponent
   },
   {
-    path:'**',
+    path: 'signup',
+    component: SignupComponent
+  },
+
+  {
+    path: '**',
     redirectTo: 'menu'
   }
 ];
